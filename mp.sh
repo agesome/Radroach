d95dc375440cf56bfffeebb51ad760105b6a0070
Evgeny Grablyk <evgeny.grablyk@gmail.com>
2009-12-30 19:47:23 +0200

    Added cflags for s-eval plugin.

--- mp.sh
+++ mp.sh
@@ -2,7 +2,7 @@
 
 for i in `ls src/plugins/ | grep ".c$" | sed -s 's/.c//g'`
 do
-gcc -Isrc/ -O1 -g -ggdb -pipe -Wall -Wextra -rdynamic -fPIC src/plugins/$i.c -c
-gcc -O1 -g -ggdb -pipe -Wall -Wextra -rdynamic -shared -o plugins/$i.so $i.o
+gcc -Isrc/ -O1 -g -ggdb -pipe -Wall -Wextra -rdynamic -lguile -lltdl -lgmp -lcrypt -lm -lltdl -fPIC src/plugins/$i.c -c
+gcc -O1 -g -ggdb -pipe -Wall -Wextra -rdynamic -lguile -lltdl -lgmp -lcrypt -lm -lltdl -shared -o plugins/$i.so $i.o
 rm $i.o
 done
