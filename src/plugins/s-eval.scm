07c3a40e01e9bdb4f20f43b700af40c04ed61f9b
Evgeny Grablyk <evgeny.grablyk@gmail.com>
2009-12-31 23:08:46 +0200

    Added sum plugin. Added forgotten s-eval.scm

---
+++ src/plugins/s-eval.scm
@@ -0,0 +1,1 @@
+;; (require scheme/sandbox)
---
+++ src/plugins/sum.c
@@ -0,0 +1,27 @@
+#include "plugins.h"
+
+float sum = 0;
+
+void
+execute (message_t * msg, command_t * cmd)
+{
+  float sum_t;
+  char *l;
+  
+  if (strstr (cmd->params, "reset"))
+    {
+      sum = 0;
+      reply (msg, "Sum reset.");
+    }
+  else if (strstr (cmd->params, "show"))
+    {
+      l = malloc (strlen ("Sum is: .") + 4);
+      sprintf (l, "Sum is: %2.1f\n", sum);
+      reply (msg, l);
+    }
+  else
+    {
+      if (sscanf (cmd->params, "%f", &sum_t))
+	sum += sum_t;
+    }
+}
