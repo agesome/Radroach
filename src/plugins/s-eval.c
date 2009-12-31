59befc84aaede192149dd06aff850b68f6404408
Evgeny Grablyk <evgeny.grablyk@gmail.com>
2009-12-31 17:35:04 +0200

    Added binding for C function `reply`

--- src/plugins/s-eval.c
+++ src/plugins/s-eval.c
@@ -4,6 +4,15 @@
 int eerror = 0;
 SCM scm, scm_result;
 char *result;
+message_t *glob_msg;
+
+SCM
+scm_reply (SCM str)
+{
+  reply (glob_msg, scm_to_locale_string (scm_object_to_string (str, SCM_UNDEFINED)));
+  
+  return SCM_BOOL_T;
+}
 
 void
 exec_error (message_t *msg)
@@ -16,7 +25,9 @@ exec_error (message_t *msg)
 
 void
 execute (message_t *msg, command_t *cmd)
-{  
+{
+  glob_msg = msg;
+  
   scm = scm_internal_catch (SCM_BOOL_T, (scm_t_catch_body) scm_c_eval_string,
 			    (void *) cmd->params, (scm_t_catch_handler) exec_error, (void *) msg);
   if (eerror)
@@ -38,4 +49,6 @@ s_eval_initialize (void)
   scm_init_guile ();
 
   scm_primitive_load (scm_from_locale_string ("./src/plugins/s-eval.scm"));
+
+  gh_new_procedure ("reply", scm_reply, 1, 0, 0);
 }
